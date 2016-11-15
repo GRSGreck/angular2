51335eb1899ae896c151b1d004ec0e223602c9cd
roman <grechukromansergeevich@gmail.com>
2016-11-15 19:20:54 +0200

    init

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,8 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
-}
-
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input type="text" [(ngModel)]="hero.name" placeholder="name">
-        </div>
-        `
+    template: '<h1>Hello Angular!</h1>'
 })
 
-export class AppComponent {
-    public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-}
+export class AppComponent { }
